b073d6cf0374ab52ebb8225b9ebf32a42b6ed376
Hristo <hristo.anastasov@gmail.com>
2021-10-15 14:56:31 +0300

    Team-Collaboration-App initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyTasksComponent } from './my-tasks/my-tasks.component';
+import { IgxAvatarModule, IgxButtonModule, IgxIconModule, IgxNavigationDrawerModule, IgxListModule, IgxCalendarModule, IgxCardModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { DashboardsComponent } from './dashboards/dashboards.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyTasksComponent,
+    DashboardsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HammerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxAvatarModule,
+    IgxButtonModule,
+    IgxIconModule,
+    IgxNavigationDrawerModule,
+    IgxListModule,
+    IgxCalendarModule,
+    FormsModule,
+    IgxCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
